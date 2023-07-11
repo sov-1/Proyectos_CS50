b71ac4c2ba5ad356e3c91d556443c5943b4a3eb1
sov-1 <jsosoriov@udistrital.edu.co>
2023-07-11 18:43:56 -0500

    add url to comment and bid to a listing

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("newListing", views.new_listing, name="newListing"),
+    path("newComment", views.new_comment, name="newComment"),
+    path("newBid", views.new_bid, name="newBid"),
     path("<int:id>", views.listing_item, name="itemListing")
 ]
