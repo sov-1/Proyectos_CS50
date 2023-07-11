29ba2a9f9f86c287b9bde87bf711f14b4ae3d1d9
sov-1 <jsosoriov@udistrital.edu.co>
2023-07-11 18:43:30 -0500

    tables added. Connected with foreing keys

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -14,6 +14,26 @@ class Listings(models.Model):
 	image = models.URLField(blank=True)
 	category = models.CharField(max_length=100)
 	dateCreated = models.DateTimeField(auto_now=True)
+	owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="creator")
         
 	def __str__(self):
 		  return f"Listing {self.id}, article {self.title}, categories: {self.category}"
+	
+class Comments(models.Model):
+	stars = models.PositiveSmallIntegerField()
+	comment = models.TextField()
+	owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name="commenter")
+	listing = models.ForeignKey(Listings, on_delete=models.CASCADE, null=True, blank=True, related_name="product")
+	date = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return f"Comment {self.id}, made by {self.owner} to Listing {self.listing}, says: '{self.comment}' on {self.date}. Rated with {self.stars} stars."
+	
+class Bids(models.Model):
+	listing = models.ForeignKey(Listings, on_delete=models.CASCADE, null=True, blank=True, related_name="offer")
+	owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name="bider")
+	bid = models.PositiveSmallIntegerField()
+
+	def __str__(self):
+		return f"User {self.owner} offer ${self.bid} on Listing: {self.listing}"
+	
