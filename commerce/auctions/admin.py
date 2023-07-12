8469c95227433d6873d821ad9ed530345a41e470
sov-1 <jsosoriov@udistrital.edu.co>
2023-07-12 16:34:16 -0500

    register all tables on admin site

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -6,6 +6,17 @@ from .models import *
 class ListingsAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "description", "image", "dateCreated")
 
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ("id", "stars", "owner", "listing")
+    
+class BidsAdmin(admin.ModelAdmin):
+    list_display = ("listing", "bid", "owner")
+    
+class WatchlistsAdmin(admin.ModelAdmin):
+    list_display = ("id", "owner", "products")
 
 admin.site.register(User)
 admin.site.register(Listings, ListingsAdmin)
+admin.site.register(Comments, CommentsAdmin)
+admin.site.register(Bids, BidsAdmin)
+admin.site.register(Watchlists, WatchlistsAdmin)
